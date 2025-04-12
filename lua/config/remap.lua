3250c91294316267afc68e29e7ce1f0b54db29c6
Josquin2 <Josquinwork@mail.ru>
2025-04-12 14:41:15 +0300

    feat: added vsplit remap

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -32,3 +32,5 @@ vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><
 vim.keymap.set("n", "<leader><leader>", function()
   vim.cmd("so")
 end)
+
+vim.keymap.set("n", "<leader>m", ":vsplit<CR>", { desc = "Vertical split", silent = true })
