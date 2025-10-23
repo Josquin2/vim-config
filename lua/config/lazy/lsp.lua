3b1f7ee686212951c430fe599768fb7d65a662cc
Josquin2 <Josquinwork@mail.ru>
2025-10-24 00:47:36 +0300

    fix: use debian route to vue lang server

--- lua/config/lazy/lsp.lua
+++ lua/config/lazy/lsp.lua
@@ -55,14 +55,12 @@ return {
         end,
 
       },
-
-
     })
 
 
-    local vue_language_server_path = '/opt/homebrew/lib/node_modules/@vue/language-server' -- macos
+    -- local vue_language_server_path = '/opt/homebrew/lib/node_modules/@vue/language-server' -- macos
     -- local vue_language_server_path = 'usr/lib/node_modules/@vue/language-server' -- linux (arch)
-    -- local vue_language_server_path = 'usr/local/lib/node_modules/@vue/language-server' -- linux (debian)
+    local vue_language_server_path = 'usr/local/lib/node_modules/@vue/language-server' -- linux (debian)
     local vue_plugin = {
       name = '@vue/typescript-plugin',
       location = vue_language_server_path,
